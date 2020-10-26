68b1da41250272e2baf7abd8343021a8ab5a77e5
Xiangru Lian <admin@mail.xrlian.com>
2020-10-27 01:30:06 +0800

    [fix] fix example service without http2

--- example-service/src/bin/server.rs
+++ example-service/src/bin/server.rs
@@ -1,5 +1,5 @@
 use example_service::Service;
-use hyper::service::{make_service_fn, };
+use hyper::service::make_service_fn;
 use hyper::Server;
 
 #[tokio::main]
@@ -8,7 +8,8 @@ async fn main() -> anyhow::Result<()> {
         make_service_fn(|_| async { Ok::<_, hyper::Error>(Service {}) });
 
     let server = Server::bind(&([0, 0, 0, 0], 8080).into())
-        .http2_only(true)
+        // .http2_only(true)
+        .tcp_nodelay(true)
         .serve(service);
 
     server.await?;
