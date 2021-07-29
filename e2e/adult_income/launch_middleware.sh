3645f1bcda695d0db4df25a8457f5cefff0c02d7
Yulong Wang <740056233@qq.com>
2021-07-30 03:03:26 +0800

    fix: execute path error && permission erro

--- e2e/adult_income/launch_middleware.sh
+++ e2e/adult_income/launch_middleware.sh
@@ -3,15 +3,6 @@ set -x
 
 shard_idx=$(($JOB_ID - 1))
 
-
-if [ $DEBUG ];then
-excute_path=/workspace/third_party/persia-embedding-sharded-middleware
-else
-excute_path=persia-embedding-sharded-middleware
-echo "add server into python module at release time"
-exit
-fi
-
-$excute_path --port $MIDDLEWARE_PORT --global-config /workspace/config/middleware_config.yml \
+persia-embedding-sharded-middleware --port $MIDDLEWARE_PORT --global-config /workspace/config/middleware_config.yml \
     --embedding-config /workspace/config/embedding_config.yml \
     --replica-index $shard_idx --replica-size $MIDDLEWARE_REPLICA
