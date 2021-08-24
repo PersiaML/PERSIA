3e7a5660aeab80d72877e3511c04f1a5c9fd4bd5
Yulong Wang <740056233@qq.com>
2021-08-24 17:41:49 +0800

    chore: update setup.py

--- rust/setup.py
+++ rust/setup.py
@@ -4,20 +4,20 @@ import os
 from setuptools import setup, find_packages
 from setuptools_rust import Binding, RustExtension
 
+
 if __name__ == "__main__":
     import colorama
     colorama.init(autoreset=True)
-    cwd = os.path.dirname(os.path.abspath(__file__))
 
     use_cuda = os.environ.get("USE_CUDA", False)
     features = None if not use_cuda else ["cuda"]
 
     setup(
         name="persia-core",
-        use_scm_version={"local_scheme": "no-local-version"},
+        use_scm_version={"local_scheme": "no-local-version", "root": "..", "relative_to": __file__},
         setup_requires=["setuptools_scm"],
-        url="https://github.com/PersiaML/PersiaML-server",
-        python_requires=">=3.6",
+        url="https://github.com/PersiaML/PersiaML/rust",
+        python_requires=">=3.7",
         description="Core Python binding for PersiaML.",
         package_dir={"": "python/"},
         packages=find_packages("python/"),
