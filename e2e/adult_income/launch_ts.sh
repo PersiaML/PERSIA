3f95b45faf7b0a619012cb68d9d9ac0cd11f67ea
Xuefeng Zhu <snowpeakz@163.com>
2021-09-07 11:06:35 +0800

    test: make ci exit when failed

--- e2e/adult_income/launch_ts.sh
+++ e2e/adult_income/launch_ts.sh
@@ -6,15 +6,18 @@ torch-model-archiver \
     --version 1.0 \
     --serialized-file $INFER_CHECKPOINT_DIR/jit_dense.pt \
     --handler /workspace/serve_handler.py \
-    --export-path $INFER_CHECKPOINT_DIR/ -f && \
+    --export-path $INFER_CHECKPOINT_DIR/ -f
+
 torchserve --start --ncs --model-store $INFER_CHECKPOINT_DIR \
     --models adult_income.mar \
-    --ts-config config/ts_config.properties & && \
+    --ts-config config/ts_config.properties &
+
 python -m grpc_tools.protoc \
     --proto_path=/workspace/proto/ \
     --python_out=/workspace/proto/ \
     --grpc_python_out=/workspace/proto/ \
-    /workspace/proto/inference.proto && \
-sleep 10s && \
-python serve_client.py && \
-torchserve --stop
+    /workspace/proto/inference.proto
+
+sleep 10s
+
+python serve_client.py && torchserve --stop
