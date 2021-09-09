ccc831ca19b4524e45b8c14101b9a074e3f6ba5d
Xiangru Lian <admin@mail.xrlian.com>
2021-09-08 22:21:46 -0700

    chore: update

--- docs/build.sh
+++ docs/build.sh
@@ -8,6 +8,7 @@ if [ "$BUILD_MULTIVERSION" == "1" ]; then
 	git checkout self-hosted-docs
 	git pull --unshallow
 	python3 -m sphinx_multiversion docs build/html
+	cp docs/index.html build/html/index.html
 else
 	python3 -m sphinx docs build/html
 fi
