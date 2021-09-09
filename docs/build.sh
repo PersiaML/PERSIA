cc523a8fa14f4c217616034fe463ad1f069d1f57
Xiangru Lian <admin@mail.xrlian.com>
2021-09-08 22:10:15 -0700

    chore: update

--- docs/build.sh
+++ docs/build.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
-git pull --unshallow
 python3 -m pip install -r docs/doc-requirements.txt
 if [ "$BUILD_MULTIVERSION" == "1" ]; then
+	git checkout main
+	git pull --unshallow
 	python3 -m sphinx_multiversion docs build/html
 else
 	python3 -m sphinx docs build/html
