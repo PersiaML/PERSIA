ba4f36212fb90c44cf399e373431df7c52d4f873
Xuefeng Zhu <37508471+snowpeakz@users.noreply.github.com>
2021-10-09 14:01:42 +0800

    fix: configurable deadlock detection thread (#120)

--- rust/persia-common/src/utils.rs
+++ rust/persia-common/src/utils.rs
@@ -19,23 +19,30 @@ impl<T> ChannelPair<T> {
 }
 
 pub fn start_deadlock_detection_thread() {
-    std::thread::spawn(move || {
-        tracing::info!("deadlock detection thread started");
-        loop {
-            std::thread::sleep(std::time::Duration::from_secs(60));
-            let deadlocks = parking_lot::deadlock::check_deadlock();
-            if deadlocks.is_empty() {
-                continue;
-            }
+    if std::env::var("PERSIA_DEADLOCK_DETECTION")
+        .unwrap_or(String::from("0"))
+        .parse::<usize>()
+        .expect("PERSIA_DEADLOCK_DETECTION should be 0 or 1")
+        > 0
+    {
+        std::thread::spawn(move || {
+            tracing::info!("deadlock detection thread started");
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(60));
+                let deadlocks = parking_lot::deadlock::check_deadlock();
+                if deadlocks.is_empty() {
+                    continue;
+                }
 
-            tracing::error!("{} deadlocks detected", deadlocks.len());
-            for (i, threads) in deadlocks.iter().enumerate() {
-                tracing::error!("Deadlock #{}", i);
-                for t in threads {
-                    tracing::error!("Thread Id {:#?}", t.thread_id());
-                    tracing::error!("{:#?}", t.backtrace());
+                tracing::error!("{} deadlocks detected", deadlocks.len());
+                for (i, threads) in deadlocks.iter().enumerate() {
+                    tracing::error!("Deadlock #{}", i);
+                    for t in threads {
+                        tracing::error!("Thread Id {:#?}", t.thread_id());
+                        tracing::error!("{:#?}", t.backtrace());
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
