2b6a55593f72558f7a9179dd50dad45ec551fd9e
Xuefeng Zhu <snowpeakz@163.com>
2021-11-15 19:10:45 +0800

    rename

--- k8s/src/bin/server.rs
+++ k8s/src/bin/server.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use kube::client::Client;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -7,6 +8,10 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let kubernetes_client: Client = Client::try_default()
+        .await
+        .expect("Expected a valid KUBECONFIG environment variable.");
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
