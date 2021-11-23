032cc60195dd72680d668e07e32300bcda27c427
Xuefeng Zhu <snowpeakz@163.com>
2021-11-23 14:31:32 +0800

    fix: env var

--- persia/service.py
+++ persia/service.py
@@ -6,7 +6,7 @@ from typing import List
 def get_embedding_worker_services() -> List[str]:
     """Get a list of current embedding worker services"""
     return (
-        [os.environ["embedding_worker_service"]]
-        if os.environ.get("embedding_worker_service", None)
+        [os.environ["EMBEDDING_WORKER_SERVICE"]]
+        if os.environ.get("EMBEDDING_WORKER_SERVICE", None)
         else ["embedding_worker:8887"]
     )
