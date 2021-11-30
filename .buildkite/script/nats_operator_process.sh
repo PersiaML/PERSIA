3df258b7f0c0cda79e4dda1576885bfec66beace
Xuefeng Zhu <snowpeakz@163.com>
2021-11-30 22:47:41 +0800

    update pre-command

--- .buildkite/script/nats_operator_process.sh
+++ .buildkite/script/nats_operator_process.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if [[ $1 == "apply" ]]; then
-    cp k8s/resources/nats.operator.temp.yaml k8s/resources/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
-    sed -i 's/persia-nats-service/persia-nats-service-'${BUILDKITE_PIPELINE_ID}'/g' k8s/resources/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
-    kubectl apply -f k8s/resources/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
+    cp ${BUILDKITE_BUILD_CHECKOUT_PATH}/k8s/resources/nats.operator.temp.yaml ${BUILDKITE_BUILD_CHECKOUT_PATH}/e2e/cache/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
+    sed -i 's/persia-nats-service/persia-nats-service-'${BUILDKITE_PIPELINE_ID}'/g' ${BUILDKITE_BUILD_CHECKOUT_PATH}/e2e/cache/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
+    kubectl apply -f ${BUILDKITE_BUILD_CHECKOUT_PATH}/e2e/cache/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
 elif [[ $1 == "delete" ]]; then
-    kubectl delete -f k8s/resources/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
-    rm k8s/resources/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
+    kubectl delete -f ${BUILDKITE_BUILD_CHECKOUT_PATH}/e2e/cache/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
+    rm ${BUILDKITE_BUILD_CHECKOUT_PATH}/e2e/cache/nats.operator.${BUILDKITE_PIPELINE_ID}.yaml
 fi
