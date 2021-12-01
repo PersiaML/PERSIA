aca8207467457ad0ddbb01a2a77d20c22da5f09b
Xuefeng Zhu <snowpeakz@163.com>
2021-12-01 11:36:35 +0800

    test_interval

--- examples/src/adult-income/train.py
+++ examples/src/adult-income/train.py
@@ -109,8 +109,8 @@ if __name__ == "__main__":
         os.path.dirname(os.path.realpath(__file__)), "data/test.npz"
     )
     test_dataset = TestDataset(test_dir, batch_size=128)
-    test_interval = 254 / world_size
-    logger.info(f"test_interval = {test_interval}, world_size = {world_size}")
+    test_interval = 254 // world_size - 1
+
     buffer_size = 10
 
     with TrainCtx(
