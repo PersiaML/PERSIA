8d9a18c189ec575d30b16e714309268314a698c1
Yulong Wang <740056233@qq.com>
2021-12-03 01:19:09 +0800

    chore: update environment name

--- persia/env.py
+++ persia/env.py
@@ -22,20 +22,20 @@ class _Env:
             self.rank = int(os.environ["RANK"])
             self.local_rank = int(os.environ["LOCAL_RANK"])
             self.world_size = int(os.environ["WORLD_SIZE"])
-            assert self.rank >= 0, "rank should not be a negative."
-            assert self.local_rank >= 0, "local_rank should not be a negative."
-            assert self.world_size >= 1, "world_size should greater than one."
+            assert self.rank >= 0, "RANK should not be a negative."
+            assert self.local_rank >= 0, "LOCAL_RANK should not be a negative."
+            assert self.world_size >= 1, "WORLD_SIZE should greater than one."
         else:
             if "REPLICA_INDEX" in os.environ:
                 self.replica_index = int(os.environ["REPLICA_INDEX"])
                 self.replica_size = int(os.environ["REPLICA_SIZE"])
                 assert (
                     self.replica_index >= 0
-                ), "replica_index should not be a negative."
-                assert self.replica_size >= 1, "replica_size should greater than one."
+                ), "REPLICA_IDNEX should not be a negative."
+                assert self.replica_size >= 1, "REPLICA_SIZE should greater than one."
             else:
                 _logger.warning(
-                    "replica_index not found, use default replica_index=0 and default replica_size=1"
+                    "REPLICA_INDEX not found, use default replica_index=0 and default replica_size=1"
                 )
                 self.replica_size = 1
                 self.replica_index = 0
