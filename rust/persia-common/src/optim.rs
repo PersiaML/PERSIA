f233c9e6879c51294410c70a7d65035c5334c44d
Xuefeng Zhu <snowpeakz@163.com>
2022-01-22 19:41:56 +0800

    try

--- rust/persia-common/src/optim.rs
+++ rust/persia-common/src/optim.rs
@@ -122,8 +122,8 @@ impl Optimizable for Adam {
     #[inline]
     fn update(&self, emb_entry: &mut [f32], grad: &[f32], dim: usize, slot_index: usize) {
         let timestep = self.timestep[slot_index].load(Acquire);
-        let beta1_power = pow(self.config.beta1, timestep as f32);
-        let beta2_power = pow(self.config.beta2, timestep as f32);
+        let beta1_power = self.config.beta1.powi(slot_index as i32);
+        let beta2_power = self.config.beta1.powi(slot_index as i32);
         let (emb, opt) = emb_entry.split_at_mut(dim);
         let (adam_m, adam_v) = opt.split_at_mut(dim);
 
